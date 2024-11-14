b245d58e7685c3683cf73bf1861081acb8686040
zth2 <zth23@ic.ac.uk>
2024-11-14 10:35:55 +0000

    first commit

---
+++ repo/rtl/register_file.sv
@@ -0,0 +1,19 @@
+module register_file (
+    input logic clk, 
+    input logic we,
+    input logic [4:0] rs1, rs2,
+    input logic [4:0] rd,
+    input logic [31:0] wd,
+    output logic [31:0] rd1, rd2
+);
+    logic [31:0] reg_file [0:31];
+
+    assign rd1 = reg_file[rs1];
+    assign rd2 = reg_file[rs2];
+
+    always_ff @(posedge clk) begin
+        if (we && rd != 0) begin
+            reg_file[rd] <= wd;
+        end
+    end
+endmodule
