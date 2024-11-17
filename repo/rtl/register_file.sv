bc2c9f11d5f98e05bfa49a9dfbd8b8e67985517a
zth2 <zth23@ic.ac.uk>
2024-11-17 16:15:53 +0000

    reg_file corrected

--- repo/rtl/register_file.sv
+++ repo/rtl/register_file.sv
@@ -1,12 +1,12 @@
 module register_file (
     input logic clk, 
     input logic we,
-    input logic [4:0] rs1, rs2,
-    input logic [4:0] rd,
-    input logic [31:0] wd,
-    output logic [31:0] rd1, rd2
+    input logic [4:0] rs1, rs2, //read register address
+    input logic [4:0] rd, //write register address
+    input logic [31:0] wd, //write data
+    output logic [31:0] rd1, rd2 // read data output
 );
-    logic [31:0] reg_file [0:31];
+    logic [31:0] reg_file [0:15];
 
     assign rd1 = reg_file[rs1];
     assign rd2 = reg_file[rs2];
