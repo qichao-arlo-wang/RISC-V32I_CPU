69748a6ea3ff6d333d4e97a5a661b2d3d168d49e
zth2 <zth23@ic.ac.uk>
2024-11-22 13:33:28 +0000

    data memory modified

--- rtl/data_mem.sv
+++ rtl/data_mem.sv
@@ -3,22 +3,34 @@ module data_mem #(
     parameter ADDR_WIDTH = 10
 ) (
     input  logic                   clk,
+    input  logic                   rst,
     input  logic                   we,
     input  logic [ADDR_WIDTH-1:0]  a,
     input  logic [DATA_WIDTH-1:0]  wd,
     output logic [DATA_WIDTH-1:0]  rd
 );
 
-    // I wrote this file in 5 min, please someone responsible for this file debug ////////////
-    logic [DATA_WIDTH-1:0] data_mem [0:(1 << ADDR_WIDTH)-1];
+    //  memory array - size determined by ADDR_WIDTH 
+    logic [DATA_WIDTH-1:0] data_mem [0:(1 << ADDR_WIDTH)-1]; 
 
-    always_ff @(posedge clk) begin
-        rd <= data_mem[a];
+    //initialise memory array to a known state (this is optional!! for simulation purposes!!!)
+    initial begin
+        for (int i = 0; i < (1 << ADDR_WIDTH); i++) begin
+            data_mem[i] = '0;
+        end
     end
 
-    always_ff @(posedge clk) begin
-        if (we) begin
-            data_mem[a] <= wd;
+    //memory read and write operations
+    always_ff @(posedge clk or posedge rst) begin //add reset signals to initialise the rd output
+        if (rst) begin
+            rd <= '0; // reset rd output
+        end else begin
+            if (we) begin
+                data_mem[a] <= wd;
+                rd <= wd; //read after write : rd reflects rewly written value
+            end else begin
+                rd <= data_mem[a];
+            end
         end
     end
 
