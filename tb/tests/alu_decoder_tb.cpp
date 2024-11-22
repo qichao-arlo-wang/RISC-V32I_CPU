39bf7959ad2e8019d5a69c9fdb2846a3c0e48d9a
lex734 <enxinglao@gmail.com>
2024-11-22 13:56:46 +0000

    alu_decoder_tb_cpp updated

--- tb/tests/alu_decoder_tb.cpp
+++ tb/tests/alu_decoder_tb.cpp
@@ -18,7 +18,7 @@ TEST_F(ALUDecoderTestBench, LW_SW) //need to change
     top->alu_op = 0;
     top->funct3 = 4;
     top->funct7_5 = 1;
-
+    top->eval();
     EXPECT_EQ(top->alu_control, 0);
 }
 
@@ -26,6 +26,7 @@ TEST_F(ALUDecoderTestBench, R_ADD){
     top->alu_op = 2;
     top->funct3 = 0;
     top->funct7_5 = 0;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 0);
 }
@@ -34,6 +35,7 @@ TEST_F(ALUDecoderTestBench, R_SUB){
     top->alu_op = 2;
     top->funct3 = 0;
     top->funct7_5 = 1;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 1);
 }
@@ -42,22 +44,24 @@ TEST_F(ALUDecoderTestBench, R_SHIFT_LEFT_LOGICAL){
     top->alu_op = 2;
     top->funct3 = 1;
     top->funct7_5 = 0;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 5);
 }
 
-TEST_F(ALUDecoderTestBench, R_SET_LESS_THAN){ //dont know how to implement this
-    top->alu_op = 2;
-    top->funct3 = 2;
-    top->funct7_5 = 0;
+// TEST_F(ALUDecoderTestBench, R_SET_LESS_THAN){ //dont know how to implement this
+//     top->alu_op = 2;
+//     top->funct3 = 2;
+//     top->funct7_5 = 0;
 
-    EXPECT_EQ(top->alu_control, 3'b)
-}
+//     EXPECT_EQ(top->alu_control, 3'b)
+// }
 
 TEST_F(ALUDecoderTestBench, R_XOR){
     top->alu_op = 2;
     top->funct3 = 4;
     top->funct7_5 = 0;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 4);
 }
@@ -66,6 +70,7 @@ TEST_F(ALUDecoderTestBench, R_SHIFT_RIGHT_LOGICAL){
     top->alu_op = 2;
     top->funct3 = 5;
     top->funct7_5 = 0;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 6);
 }
@@ -74,16 +79,27 @@ TEST_F(ALUDecoderTestBench, R_OR){
     top->alu_op = 2;
     top->funct3 = 6;
     top->funct7_5 = 0;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 3);
 }
 
-TEST_F(ALUDecoderTestBench, R_ADD){
+TEST_F(ALUDecoderTestBench, R_AND){
     top->alu_op = 2;
     top->funct3 = 7;
     top->funct7_5 = 0;
+    top->eval();
 
     EXPECT_EQ(top->alu_control, 2);
+}
+
+TEST_F(ALUDecoderTestBench, DEFAULT_CASE) {
+    top->alu_op = 3;  // Invalid ALU operation
+    top->funct3 = 0;
+    top->funct7_5 = 0;
+    top->eval();
+
+    EXPECT_EQ(top->alu_control, 0);  // Default case
 }
 
 int main(int argc, char **argv)
