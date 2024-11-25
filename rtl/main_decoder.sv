48ffceb76ba88a1242b1b224886289b3dbeff780
Keven Zhu <kevenzhu@KevendeMacBook-Pro.local>
2024-11-25 23:46:01 +0000

    update imm_src for main_decoder

--- rtl/main_decoder.sv
+++ rtl/main_decoder.sv
@@ -2,7 +2,7 @@ module main_decoder (
     input logic [6:0] opcode_i,    // Opcode from instruction
     output logic reg_wr_en_o,      // Register Write Enable
     output logic mem_wr_en_o,      // Memory Write Enable
-    output logic [1:0] imm_src_o,  // Immediate source control
+    output logic [2:0] imm_src_o,  // Immediate source control
     output logic alu_src_o,        // ALU source (register or immediate)
     output logic branch_o,         // Branch control
     output logic result_src_o,     // Result source (ALU or memory)
@@ -13,7 +13,7 @@ module main_decoder (
         // default values for all outputs
         reg_wr_en_o = 0;
         mem_wr_en_o = 0;
-        imm_src_o = 2'b00;
+        imm_src_o = 3'b000;
         alu_src_o = 0;
         branch_o = 0;
         result_src_o = 0;
@@ -25,7 +25,7 @@ module main_decoder (
             7'b0000011: begin
                 reg_wr_en_o = 1;
                 mem_wr_en_o = 0;
-                imm_src_o = 2'b00;
+                imm_src_o = 3'b000;
                 alu_src_o = 1;
                 result_src_o = 1;
                 alu_op_o = 2'b00;
@@ -35,16 +35,27 @@ module main_decoder (
             7'b0010011: begin
                 reg_wr_en_o = 1;
                 mem_wr_en_o = 0;
-                imm_src_o = 2'b00;
                 alu_src_o = 1;
                 alu_op_o = 2'b10;
+
+                case (funct_3_i)
+                    // SLLI
+                    3'b001: begin
+                        imm_src_o = 3'b101;
+                    end
+                    // SRLI/SRAI
+                    3'b101: begin
+                        imm_src_o = 3'b101;
+                    end
+                    default:
+                        imm_src_o = 3'b000;
             end
 
             // S-type, op = 35
             7'b0100011: begin
                 reg_wr_en_o = 0;
                 mem_wr_en_o = 1;
-                imm_src_o = 2'b01;
+                imm_src_o = 3'b001;
                 alu_src_o = 1;
                 alu_op_o = 2'b00;
             end
@@ -61,7 +72,7 @@ module main_decoder (
             7'b1100011: begin
                 reg_wr_en_o = 0;
                 mem_wr_en_o = 0;
-                imm_src_o = 2'b10;
+                imm_src_o = 3'b010;
                 alu_src_o = 0;
                 branch_o = 1;
                 alu_op_o = 2'b01;
