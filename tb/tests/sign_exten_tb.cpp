67b9b8a1a0da7866a7988dffdf37548afeef0d73
lex734 <enxinglao@gmail.com>
2024-11-26 01:22:42 +0000

    sign_exten_tb.cpp completed

--- tb/tests/sign_exten_tb.cpp
+++ tb/tests/sign_exten_tb.cpp
@@ -18,7 +18,7 @@ TEST_F(SignExtenTestbench, I_TYPE_POS)
     top->imm_src_i = 0;
     top->eval();
 
-    top->imm_ext_o = 2781;
+    EXPECT_EQ(top->imm_ext_o, 695);
 }
 
 TEST_F(SignExtenTestbench, I_TYPE_NEG)
@@ -27,7 +27,7 @@ TEST_F(SignExtenTestbench, I_TYPE_NEG)
     top->imm_src_i = 0;
     top->eval();
 
-   top->imm_ext_o = -1315; //4294967150
+    EXPECT_EQ(top->imm_ext_o, -1315);
 }
 
 TEST_F(SignExtenTestbench, S_TYPE)
@@ -36,9 +36,58 @@ TEST_F(SignExtenTestbench, S_TYPE)
     top->imm_src_i = 1;
     top->eval();
 
-   top->imm_ext_o = -146;
+    EXPECT_EQ(top->imm_ext_o, 1134);
 }
 
+TEST_F(SignExtenTestbench, S_TYPE_NEG)
+{
+    top->instr_31_7_i = 0b1101001010101000000101010;
+    top->imm_src_i = 1;
+    top->eval();
+
+    EXPECT_EQ(top->imm_ext_o, -726);
+}
+
+TEST_F(SignExtenTestbench, U_TYPE){
+    top->instr_31_7_i = 0b1101001010101000000101010;
+    top->imm_src_i = 3;
+    top->eval();
+
+    EXPECT_EQ(top->imm_ext_o, 27611168);
+}
+
+TEST_F(SignExtenTestbench, B_TYPE){
+    top->instr_31_7_i = 0b0101001010101000000101010;
+    top->imm_src_i = 2;
+    top->eval();
+
+    EXPECT_EQ(top->imm_ext_o, 645);
+}
+
+TEST_F(SignExtenTestbench, B_TYPE_NEG){
+    top->instr_31_7_i = 0b1111100000100000011000001;
+    top->imm_src_i = 2;
+    top->eval();
+
+    EXPECT_EQ(top->imm_ext_o, -4);
+}   
+
+TEST_F(SignExtenTestbench, J_TYPE){
+    top->instr_31_7_i = 0b0101101011010100010000001;
+    top->imm_src_i = 4;
+    top->eval();
+
+    EXPECT_EQ(top->imm_ext_o, 177236);
+}
+
+TEST_F(SignExtenTestbench, J_TYPE_NEG){
+    top->instr_31_7_i = 0b1111010110011111111100001;
+    top->imm_src_i = 4;
+    top->eval();
+
+    EXPECT_EQ(top->imm_ext_o, -340);
+
+}
 int main(int argc, char **argv)
 {
     top = new Vdut;
