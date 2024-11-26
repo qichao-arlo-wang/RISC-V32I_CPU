2712b2538c97234635b549e9e0c2facd30553345
arlo-wang <arlowang23@gmail.com>
2024-11-26 11:28:21 +0000

    resiter_file update

--- rtl/register_file.sv
+++ rtl/register_file.sv
@@ -3,11 +3,11 @@ module register_file #(
     parameter REG_WIDTH = 32
 ) (
     input logic clk, 
-    input logic we3_i,
-    input logic [$clog2(REG_COUNT)-1:0] a1_i, a2_i, a3_i, // READ (a1_i, a2_i) and write (a3_i) addresses
-    input logic [REG_WIDTH-1:0] wd3_i,
+    input logic reg_wr_en_i,
+    input logic [$clog2(REG_COUNT)-1:0] rd_addr1_i, rd_addr2_i, wr_addr_i, // READ (read_addr1_i, read_addr2_i) and write (write_addr_i) addresses
+    input logic [REG_WIDTH-1:0] wr_data_i,
 
-    output logic [REG_WIDTH-1:0] rd1_o, rd2_o // read data output
+    output logic [REG_WIDTH-1:0] rd_data1_o, rd_data2_o // read data output
 );
     // declare register file
     logic [REG_WIDTH-1:0] reg_file [0:REG_COUNT-1];
@@ -22,15 +22,15 @@ module register_file #(
 
     // write data into the register file on the rising edge of the clock
     always_ff @(posedge clk) begin
-        if (we3_i && a3_i != 0) begin
-            reg_file[a3_i] <= wd3_i;
+        if (reg_wr_en_i && wr_addr_i != 0) begin
+            reg_file[wr_addr_i] <= wr_data_i;
         end
         reg_file[0] <= 32'b0; // ensure register 0 always holds the value 0
     end
 
     // Asynchronous read operation (use = rather than <=)
     always_comb begin
-        rd1_o = reg_file[a1_i];
-        rd2_o = reg_file[a2_i];
+        rd_data1_o = reg_file[rd_addr1_i];
+        rd_data2_o = reg_file[rd_addr2_i];
     end
 endmodule
