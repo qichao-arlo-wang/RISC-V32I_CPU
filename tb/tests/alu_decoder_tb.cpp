dbe2b2f0a7e39c21d4d7a6a994ac04c4a5636dae
zth2 <zth23@ic.ac.uk>
2024-11-29 14:41:37 +0000

    alu decoder tb passed

--- tb/tests/alu_decoder_tb.cpp
+++ tb/tests/alu_decoder_tb.cpp
@@ -1,96 +1,80 @@
 #include "base_testbench.h"
+#include "Vdut.h"
+#include "gtest/gtest.h"
 
 Vdut *top;
 
-class ALUDecoderTestBench : public BaseTestbench
-{
+class ALUDecoderTestBench : public BaseTestbench {
 protected:
-    void initializeInputs() 
-    {
-        top->alu_op_i = 3;
-        top->funct3_i = 7;
-        top->funct7_i = 1;
+    void initializeInputs() {
+        top->alu_op_i = 0;
+        top->funct3_i = 0;
+        top->funct7_i = 0;
     }
 };
 
-TEST_F(ALUDecoderTestBench, LW_SW)
-{
-    top->alu_op_i = 0;
-    top->funct3_i = 4;
-    top->funct7_i = 1;
+TEST_F(ALUDecoderTestBench, LW_SW) {
+    top->alu_op_i = 3; // 2'b11 for load/store instructions
+    top->funct3_i = 2; // LW/SW
+    top->funct7_i = 0;
     top->eval();
-    EXPECT_EQ(top->alu_control_o, 0);
+    EXPECT_EQ(top->alu_control_o, 0); // ADD operation
 }
 
-TEST_F(ALUDecoderTestBench, R_ADD){
+TEST_F(ALUDecoderTestBench, R_ADD) {
     top->alu_op_i = 2;
     top->funct3_i = 0;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 0);
+    EXPECT_EQ(top->alu_control_o, 0); // ADD operation
 }
 
-TEST_F(ALUDecoderTestBench, R_SUB){
+TEST_F(ALUDecoderTestBench, R_SUB) {
     top->alu_op_i = 2;
     top->funct3_i = 0;
-    top->funct7_i = 1;
+    top->funct7_i = 32; // 7'h20
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 1);
+    EXPECT_EQ(top->alu_control_o, 1); // SUB operation
 }
 
-TEST_F(ALUDecoderTestBench, R_SHIFT_LEFT_LOGICAL){
+TEST_F(ALUDecoderTestBench, R_SHIFT_LEFT_LOGICAL) {
     top->alu_op_i = 2;
     top->funct3_i = 1;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 5);
+    EXPECT_EQ(top->alu_control_o, 2); // SLL operation
 }
 
-// TEST_F(ALUDecoderTestBench, R_SET_LESS_THAN){ //dont know how to implement this
-//     top->alu_op_i = 2;
-//     top->funct3_i = 2;
-//     top->funct7_i = 0;
-
-//     EXPECT_EQ(top->alu_control_o, 3'b)
-// }
-
-TEST_F(ALUDecoderTestBench, R_XOR){
+TEST_F(ALUDecoderTestBench, R_XOR) {
     top->alu_op_i = 2;
     top->funct3_i = 4;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 4);
+    EXPECT_EQ(top->alu_control_o, 8); // XOR operation
 }
 
-TEST_F(ALUDecoderTestBench, R_SHIFT_RIGHT_LOGICAL){
+TEST_F(ALUDecoderTestBench, R_SHIFT_RIGHT_LOGICAL) {
     top->alu_op_i = 2;
     top->funct3_i = 5;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 6);
+    EXPECT_EQ(top->alu_control_o, 5); // SRL operation
 }
 
-TEST_F(ALUDecoderTestBench, R_OR){
+TEST_F(ALUDecoderTestBench, R_OR) {
     top->alu_op_i = 2;
     top->funct3_i = 6;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 3);
+    EXPECT_EQ(top->alu_control_o, 7); // OR operation
 }
 
-TEST_F(ALUDecoderTestBench, R_AND){
+TEST_F(ALUDecoderTestBench, R_AND) {
     top->alu_op_i = 2;
     top->funct3_i = 7;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 2);
+    EXPECT_EQ(top->alu_control_o, 9); // AND operation
 }
 
 TEST_F(ALUDecoderTestBench, DEFAULT_CASE) {
@@ -98,12 +82,10 @@ TEST_F(ALUDecoderTestBench, DEFAULT_CASE) {
     top->funct3_i = 0;
     top->funct7_i = 0;
     top->eval();
-
-    EXPECT_EQ(top->alu_control_o, 0);  // Default case
+    EXPECT_EQ(top->alu_control_o, 0);  // Default to ADD operation
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     top = new Vdut;
 
     testing::InitGoogleTest(&argc, argv);
