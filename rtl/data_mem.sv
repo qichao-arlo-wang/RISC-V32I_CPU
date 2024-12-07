4f32ed7df8a7603b5adbb3edb4d536253bae9f51
arlo-wang <arlowang23@gmail.com>
2024-12-07 15:39:25 +0000

    data_mem bug fixed

--- rtl/data_mem.sv
+++ rtl/data_mem.sv
@@ -15,8 +15,9 @@ module data_mem (
     // pdf_array from 0x0000 0100 to 0x0000 01FF
     // reserved mem from 0x0000 0000 to 0x0000 00FF
     localparam DATA_MEM_SIZE = 128 * 1024; // 128KB
-    logic [31:0] mem [0:DATA_MEM_SIZE-1];
+    logic [7:0] mem [DATA_MEM_SIZE-1:0];
     localparam string MEM_FILE  = "data.hex";        // Memory initialization file
+    // localparam string MEM_FILE  = "data_mem_test.hex";        // Memory initialization file
 
     // Initialize data_arrary
     initial begin
@@ -33,11 +34,19 @@ module data_mem (
         if (wr_en_i) begin
             case (byte_en_i)
                 // byte (8 bits)
-                4'b0001: mem[addr_i][7:0]  <= wr_data_i[7:0];
+                4'b0001: mem[addr_i] <= wr_data_i[7:0];
                 // half word (16 bits)
-                4'b0011: mem[addr_i][15:0] <= wr_data_i[15:0];
+                4'b0011: begin
+                    mem[addr_i]   <= wr_data_i[7:0];
+                    mem[addr_i+1] <= wr_data_i[15:8];
+                end
                 // word (32 bits)
-                4'b1111: mem[addr_i]       <= wr_data_i;
+                4'b1111: begin
+                    mem[addr_i]   <= wr_data_i[7:0];
+                    mem[addr_i+1] <= wr_data_i[15:8];
+                    mem[addr_i+2] <= wr_data_i[23:16];
+                    mem[addr_i+3] <= wr_data_i[31:24];
+                end
                 default: $display("Warning: Unrecognized byte enable: %b. No data written.", byte_en_i);
             endcase
         end
@@ -58,9 +67,9 @@ module data_mem (
     // Asynchronous read logic
     always_comb begin
         case (byte_en_i)
-            4'b0001: rd_data_o = {24'b0, mem[addr_i][7:0]};
-            4'b0011: rd_data_o = {16'b0, mem[addr_i][15:0]};
-            4'b1111: rd_data_o = mem[addr_i][31:0];
+            4'b0001: rd_data_o = {24'b0, mem[addr_i]};
+            4'b0011: rd_data_o = {16'b0, mem[addr_i+1], mem[addr_i]};
+            4'b1111: rd_data_o = {mem[addr_i+3], mem[addr_i+2], mem[addr_i+1], mem[addr_i]};
             default: rd_data_o = 32'hDEADBEEF;
         endcase
     end
